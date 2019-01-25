3b05403edcd9ec4d572d44fcd6d9cfe4f79d74d8
github username <github email address>
2019-01-25 17:53:34 -0500

    added graph styling

--- src/components/VizBeforeGraph.js
+++ src/components/VizBeforeGraph.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import folderIcon from '../icons/file-folder-icon.png'
 
 class VizBeforeGraph extends Component {
   render() {
     return (
       <div>
-        VizBeforeGraph
+        <div className='viz-before-graph'>
+          <div className='graph-container-shaddow'>
+            <div className='graph-shaddow'>
+              <div className='graph-negative-space'></div>
+            </div>
+          </div>
+          <div className='icon-container'>
+            <img className='folder-icon' src={folderIcon} alt=''></img>
+            <div className='viz-before-graph-description'>
+              Please input desired allocation into available strategies to enable analysis and further actions.
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
